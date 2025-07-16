2f52890d33ba463d64172ddd5a1b3db9ff14ea23
Imtiaz <mdimtiazzihad@gmail.com>
2025-07-17 04:21:10 +0600

    Add user registration functionality with HTTP status codes

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,1 +1,27 @@
- 
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { User } from "./user.model";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.create({
+      name,
+      email,
+    });
+
+    res.status(httpStatus.CREATED).json({
+      message: "User created successfully",
+      user,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: "Internal Server Error" });
+  }
+};
+
+export const UserController = {
+  createUser,
+};
