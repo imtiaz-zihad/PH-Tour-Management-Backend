5661f48fcbd2e3dcc614cb055df90e89afd296d4
Imtiaz <mdimtiazzihad@gmail.com>
2025-11-27 09:38:05 +0600

    Add user validation middleware and schemas for registration and updates

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,16 @@
- import { Router } from "express";
+import { createUserZodSchema } from "./user.validation";
+import { Router } from "express";
 import { UserController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
 
 const router = Router();
 
-router.post("/register", UserController.createUser);
-router.get("/all-users",UserController.getAllUsers);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserController.createUser
+);
+
+router.get("/all-users", UserController.getAllUsers);
 
 export const UserRoutes = router;
